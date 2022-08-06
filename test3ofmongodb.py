49f663dadec5ddb33e72f771a27f54c010174cf8
Sampathnani <sampath3759@gmail.com>
2022-08-06 05:51:57 +0530

    this code from json

--- test3ofmongodb.py
+++ test3ofmongodb.py
@@ -10,36 +10,20 @@ d={
     "surname": "nani",
     "email": "sampath@gmail.com"
 }
-d={
-    "name": "sampath",
-    "surname": "nani",
-    "email": "sampath@gmail.com"
-}
-d={
-    "name": "sampath",
-    "surname": "nani",
-    "email": "sampath@gmail.com"
-}
-d={
-    "name": "sampath",
-    "surname": "nani",
-    "email": "sampath@gmail.com"
-}
-d={
-    "name": "sampath",
-    "surname": "nani",
-    "email": "sampath@gmail.com"
-}
-d={
-    "name": "sampath",
-    "surname": "nani",
-    "email": "sampath@gmail.com"
-}
-d={
-    "name": "sampath",
-    "surname": "nani",
-    "email": "sampath@gmail.com"
+data={
+  "InsuranceCompanies":  {
+    "Top Insurance Companies":[
+      {
+        "No": "1",
+        "Name": "Berkshire Hathaway ( BRK.A)",
+        "Market Capitalization": "$655 billion"
+      }
+    ],
+  "source": "investopedia.com",
+  "Time":"May 2021"
+  }
 }
 db1=client['mongotest'];
 coll=db1['test'];
-coll.insert_one(d);
+##coll.insert_one(d);
+coll.insert_one(data)
